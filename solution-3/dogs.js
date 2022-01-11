19fed15df2aa7587b94c4618b19f3e6a00c93173
Paolo <paolo.ghidoni82@gmail.com>
2022-01-11 20:24:28 +0000

    correct files to upload

--- solution-3/dogs.js
+++
@@ -1,8 +0,0 @@
-let dogs = {
-  alphonso: { name: "Alphonso", breed: "German Shepherd" },
-  lassie: { name: "Lassie", breed: "Golden Retriever" },
-  pongo: { name: "Pongo", breed: "Dalmation" },
-  luna: { name: "Luna", breed: "Cocker Spaniel" },
-};
-
-module.exports = dogs;
--- solution-5/routes/deleteDog.js
+++
@@ -1,9 +0,0 @@
-const dogs = require("../dogs.js");
-
-function post(request, response) {
-  const nameToDelete = request.body.name.toLowerCase();
-  delete dogs[nameToDelete];
-  response.redirect("/");
-}
-
-module.exports = { post };
